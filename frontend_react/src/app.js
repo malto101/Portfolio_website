c478c7c585ff6e382ca86067cf027700a880b37c
melta101 <dhruvmenon1104@gmail.com>
2022-11-05 14:16:39 +0530

    nothing much day-2

---
+++ frontend_react/src/app.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
+const App = () => {
+    return (
+        <div> classroom = "app"
+            <Navbar/>
+            <Header/>
+            <About/>
+            <Footer/>
+            <Testimonial/>
+            <Work/>
+            <skills/>
+        </div>
+
+    );
+
+}
+
+export default App;
